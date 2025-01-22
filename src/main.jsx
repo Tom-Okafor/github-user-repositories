65fc3fa21091a4a6cb6f49e04efcae05ef062ca1
Tom Okafor <tmeka008@gmail.com>
2025-01-22 18:36:21 +0100

    feat: Set up Apollo Client. Create .env file to protect token.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
-createRoot(document.getElementById('root')).render(
+const HTTP_LINK = createHttpLink({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+  },
+});
+
+const CLIENT = new ApolloClient({
+  link: HTTP_LINK,
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={CLIENT}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);

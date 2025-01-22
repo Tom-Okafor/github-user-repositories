3a7888be9174c38e46abfe8aea941540426d7f59
Tom Okafor <tmeka008@gmail.com>
2025-01-22 20:19:37 +0100

    feat: use Apollo Client to fetch github user details in graphql format.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
+import { gql, useLazyQuery } from "@apollo/client";
+import { useState } from "react";
+
+const GET_USER_DETAILS = gql`
+  query getUserDetails($username: String!) {
+    user(login: $username) {
+      avatarUrl
+      name
+      bio
+      url
+      email
+      location
+      pronouns
+      repositories(first: 10) {
+        totalCount
+        nodes {
+          description
+          url
+          id
+          languages(first: 5) {
+            nodes {
+              color
+              name
+            }
+          }
+          name
+        }
+      }
+    }
+  }
+`;
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [getUserDetails, { loading, error, data }] =
+    useLazyQuery(GET_USER_DETAILS);
+  const handleSubmit = () => {
+    getUserDetails({ variables: { username: inputValue } });
+  };
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center gap-8">
       <h1 className="text-yellow-400 text-5xl font-bold font-serif">
         Github User
       </h1>
-      <form className="space-x-4">
+      <form
+        className="space-x-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
         <input
           type="text"
           placeholder="Please, enter github username."
           className="border-2 border-teal-600 rounded-lg w-[300px] px-4 py-2 [box-shadow:0px_2px_8px_-1px_#999] focus:border-2.5 duration-300 focus:border-yellow-600 focus:outline-0"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
         />
         <input
           type="submit"

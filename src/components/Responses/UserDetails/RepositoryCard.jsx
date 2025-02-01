a9f6649ce5b7c63a9dec152f634632d451c7554c
Tom Okafor <tmeka008@gmail.com>
2025-02-01 12:08:24 +0100

    refactor: create new component for code symbol svg and import into Repository card.

--- src/components/Responses/UserDetails/RepositoryCard.jsx
+++ src/components/Responses/UserDetails/RepositoryCard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import Language from "./Language";
+import CodeSvg from "./CodeSvg";
 
 export default function RepositoryCard({
   name,
@@ -12,31 +13,7 @@ export default function RepositoryCard({
 }) {
   return (
     <div className="rounded-2xl shadow-[0_0_8px_#74fce4ae] px-8 py-8 text-white relative flex gap-5 repobox bg-slate-800 border-box">
-      <svg
-        height="50px"
-        width="50px"
-        version="1.1"
-        id="Layer_1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        xmlSpace="preserve"
-        className="shrink-0"
-      >
-        <circle
-          className="has-repobox:even:fill-pink-600 has-repobox:odd:fill-yellow-400 circle"
-          cx="256"
-          cy="256"
-          r="256"
-        />
-        <g className="g">
-          <path
-            d="M213.12,319.776L99.872,270.544V243.28l113.248-49.008v32.112l-79.008,30.208l79.008,31.328V319.776
-		z"
-          />
-          <path d="M223.6,341.408l40.912-170.832h23.776l-41.36,170.832H223.6z" />
-          <path d="M298.768,319.904V288l79.104-31.104l-79.104-30.752V194.48l113.36,49.008v27.04L298.768,319.904z" />
-        </g>
-      </svg>
+      <CodeSvg />
       <ul className="font-serif space-y-[20px] w-full">
         <li className="flex justify-between">
           <span className="capitalize name font-bold text-lg">
